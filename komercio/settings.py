c1899947c9fb6f9ff7994383cca4631464a894f1
Guilherme <guilherme.llopreti@gmail.com>
2022-08-15 00:16:45 -0300

    heroku config
    Signed-off-by: Guilherme <guilherme.llopreti@gmail.com>

--- komercio/settings.py
+++ komercio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["komercio-guilopreti.herokuapp.com", "localhost"]
 
 
 # Application definition
